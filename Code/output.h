929e49e75bcf9dca3316f18d20184923c0d4d10f
Infernape4761 <howbrokenwashe@gmail.com>
2024-12-01 07:34:28 +0000

    Minor corrections in output.h

--- Code/output.h
+++ Code/output.h
@@ -24,18 +24,18 @@ public:
     int uldNumber, packageNumber; // These are the package number and uldNumber in which the package is stored
 
     // Constructor definition
-    OUTPUT(int packageNumber){
+    OUTPUT_ROW(int packageNumber){
         bottomLeft.resize(3, 0); // Size is three because we are dealing with 3D space
         topRight.resize(3, 0);
         uldNumber = -1; // Initially no ULD is assigned to the package
         this->packageNumber = packageNumber; // The package Number is updated for the package whose output is begin created.
     }
-    void updateOutput();
+    void updateOutput(vector<int> bottomLeft, vector<int> topRight, int uldNumber);
 };
 
 void OUTPUT_ROW::updateOutput(vector<int> bottomLeft, vector<int> topRight, int uldNumber){
     this->bottomLeft = bottomLeft;
-    this->topright = topRight;
+    this->topRight = topRight;
     this->uldNumber = uldNumber;
 }
 
